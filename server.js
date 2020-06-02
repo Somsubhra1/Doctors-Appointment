f1d42ae57ed7f3a9d2c432581eadcc2258da71e0
Somsubhra Das <das.somsubhra1@gmail.com>
2020-06-02 22:25:07 +0530

    Making app production ready

--- server.js
+++ server.js
@@ -26,10 +26,6 @@ mongoose
   .then(() => console.log(`MongoDB connected successfully`))
   .catch((err) => console.log(`Error connecting mongodb ` + err));
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -37,6 +33,15 @@ app.use("/auth", require("./routes/auth"));
 app.use("/appointments", require("./routes/appointment"));
 app.use("/admin", require("./routes/admin"));
 app.use("/doctors", require("./routes/doctors"));
+
+// Serve static assets if app is in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
