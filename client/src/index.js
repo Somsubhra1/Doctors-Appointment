eca394476c7514663900638dbfa2c81296dccec2
Somsubhra Das <das.somsubhra1@gmail.com>
2020-06-05 21:50:58 +0530

    updates made for pwa

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
