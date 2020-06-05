fcb0fb2253817ae264e4a72022a47ec7b576e43c
Somsubhra Das <das.somsubhra1@gmail.com>
2020-06-05 22:11:48 +0530

    Updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,8 +4,8 @@ import { NavbarBrand } from "reactstrap";
 const Footer = () => {
   return (
     <footer className="p-2">
-      <NavbarBrand color="dark" style={{ color: "#fff"}}>
-        &copy; Copyright AppName {new Date().getFullYear()}
+      <NavbarBrand color="dark" style={{ color: "#fff" }}>
+        &copy; Copyright Doctors Appointment App {new Date().getFullYear()}
       </NavbarBrand>
     </footer>
   );
